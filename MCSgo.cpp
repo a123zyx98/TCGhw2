7b8216551d4c4b3bc68473e8f9810077e27105f7
a123zyx98 <a123zyx98@gmail.com>
2015-12-24 22:12:22 +0800

    random make sure

--- MCSgo.cpp
+++ MCSgo.cpp
@@ -911,6 +911,7 @@ int main(int argc, char* argv[]) {
 //    int type = GTPVERSION;// 1: local version, 2: gtp version
     int type = GTPVERSION;// 1: local version, 2: gtp version
     int display = 0; // 1: display, 2 nodisplay
+    srand(time(NULL));
     if (argc > 1) {
     if (strcmp(argv[1], "-display")==0) {
         display = 1;
